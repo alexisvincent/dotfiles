465d8cc2fa529486d8b788fd0c0e2f3984a00084
Alexis Vincent <alexisjohnvincent@gmail.com>
2014-08-27 14:31:55 +0200

    Removed source to bashprofile

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ git pull origin master;
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "Brewfile" --exclude "Caskfile" \
-        --exclude "bin/" --exclude "init/" \
+        --exclude "bin/" --exclude "init/" --exclude "GeekTool/" --exclude "init/" \
         --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
