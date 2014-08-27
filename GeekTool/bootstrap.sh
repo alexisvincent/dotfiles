7e932c7f561e6744217fd48a87ced9370105da6b
Alexis Vincent <alexisjohnvincent@gmail.com>
2014-08-27 14:31:03 +0200

    Added Geektool config

---
+++ GeekTool/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+ln -s org.tysoe.geeklet.file.plist $HOME/Library/Preferences/
+ln -s org.tysoe.geeklet.image.plist $HOME/Library/Preferences/
+ln -s org.tysoe.geeklet.shell.plist $HOME/Library/Preferences/
+ln -s org.tysoe.GeekTool.plist $HOME/Library/Preferences/
+ln -s org.tysoe.geektool3.plist $HOME/Library/Preferences/
