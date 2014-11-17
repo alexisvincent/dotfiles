3820f28596a0bcb936b28fae887a74d93c544b54
Alexis Vincent <alexisjohnvincent@gmail.com>
2014-11-17 18:38:36 +0200

    Changed composer and artisan to use HHVM

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,5 @@ git pull origin master;
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "Brewfile" --exclude "Caskfile" \
         --exclude "bin/" --exclude "init/" --exclude "geektool/" \
-		--exclude ".osx" \
+		--exclude ".osx" --exclude "config/" \
         --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
